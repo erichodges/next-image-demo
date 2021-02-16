6edfb31e88d9d3c16992da2d584dd71e705d91e8
Eric Hodges <ehodges2004@gmail.com>
2021-02-16 15:55:30 -0800

    Setup centered text over image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,22 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div>
-      <div style={{ width: '100vw' }}>
+    <div className=''>
+      <div className='relative z-0 w-screen'>
         <Image
+          className='object-cover object-center pointer-events-none'
           src='https://res.cloudinary.com/dn3baky3u/image/upload/v1580246844/gatsby-cloudinary/evora-gt-side.jpg'
           width={2400}
           height={1200}
           layout='responsive'
         />
       </div>
-      <div>Test Div for text content over image</div>
+      <div
+        className='absolute inset-0 z-10 w-screen text-center'
+        style={{ top: '50%' }}
+      >
+        text over image
+      </div>
     </div>
   );
 }
