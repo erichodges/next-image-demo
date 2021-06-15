3dfbebb8adb00af4b970ffcbb5172da9d2d6b6df
Eric Hodges <ehodges2004@gmail.com>
2021-06-15 13:44:40 -0700

    Updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <div>
-        <h2>Responsive</h2>
+        <h2>Yo! Responsive</h2>
         <Image
           cloudName='dn3baky3u'
           publicId='v1576797449/gatsby-cloudinary/evora-gt-front'
