0c7c3cdec7f2f40f13b6b49ad40f55503813b15b
Eric Hodges <ehodges2004@gmail.com>
2021-06-20 15:03:37 -0700

    Added about page

---
+++ pages/about.tsx
@@ -0,0 +1,16 @@
+import { Image } from 'cloudinary-react';
+
+export default function about() {
+  return (
+    <div>
+      <div>
+        <h2>Responsive</h2>
+        <Image
+          cloudName='dn3baky3u'
+          publicId='v1576797449/gatsby-cloudinary/evora-gt-front'
+          width='auto'
+        />
+      </div>
+    </div>
+  );
+}
