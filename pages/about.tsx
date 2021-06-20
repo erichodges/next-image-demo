80db5e45aef2e1ac775476af62e3dd42421cc255
Eric Hodges <ehodges2004@gmail.com>
2021-06-20 15:09:50 -0700

    updated about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,12 +4,12 @@ export default function about() {
   return (
     <div>
       <div>
-        <h2>Responsive</h2>
+        {/* <h2>Responsive</h2>
         <Image
           cloudName='dn3baky3u'
           publicId='v1576797449/gatsby-cloudinary/evora-gt-front'
           width='auto'
-        />
+        /> */}
       </div>
     </div>
   );
